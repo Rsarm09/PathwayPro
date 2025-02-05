35b9aa4c30007bc01d36ecfcd1bc192dd5f6b91c
Rsarm09 <rnasarmiento@gmail.com>
2025-02-04 21:24:37 -0800

    Update HeroBanner.jsx

--- pathwaypro/src/components/HeroBanner.jsx
+++ pathwaypro/src/components/HeroBanner.jsx
@@ -9,6 +9,7 @@ const HeroBanner = () => {
 
             {/* Content */}
             <div className="relative z-10 px-4 text-white">
+                
                 {/* Headline */}
                 <h1 className="text-3xl md:text-5xl font-bold mb-4">
                     Discover Your Path to Becoming a Pro
