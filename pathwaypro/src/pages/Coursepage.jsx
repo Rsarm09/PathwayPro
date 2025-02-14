a2b81dfe7b5ada20a28830887f3a0ad6a9d721a1
Rsarm09 <rnasarmiento@gmail.com>
2025-02-14 10:20:24 -0800

    Update Coursepage.jsx

--- pathwaypro/src/pages/Coursepage.jsx
+++ pathwaypro/src/pages/Coursepage.jsx
@@ -12,7 +12,7 @@ const Coursepage = () => {
 
                 {/* Course Content Description */}
                 <section className='py-15'>
-                    <h3 className='mainTitle pb-5'>What You Will Be Learning</h3>
+                    <h3 className='mainTitle'>What You Will Be Learning</h3>
                     <p className='text-lg text-gray-700'>
                         In this beginner-friendly course, you’ll explore the fundamentals of Python step by step! From understanding variables and data types to writing loops, functions, and basic algorithms, we make coding simple and fun. You’ll also get hands-on experience with real-world exercises, so you can confidently write your own Python programs by the end. Whether you want to automate tasks, analyze data, or just learn a new skill, this course is the perfect starting point!
                     </p>
