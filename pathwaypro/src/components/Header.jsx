ee3745f9f2ff4bd36e29d28d95288c637ef6b731
Nancy W <159850766+nancying8@users.noreply.github.com>
2025-02-14 17:05:24 -0800

    new header update
    new

--- pathwaypro/src/components/Header.jsx
+++ pathwaypro/src/components/Header.jsx
@@ -1,64 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-
-import { useState } from "react";
 import { FaSearch, FaBell, FaUserCircle, FaShoppingCart, FaBars, FaTimes, FaGlobe } from "react-icons/fa";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="bg-bluedark shadow-md py-3 px-6 flex items-center justify-between">
+    <header className="bg-bluedark h-22 shadow-md py-4 px-8 flex items-center justify-between">
 
       {/* Logo */}
       <div className="flex items-center justify-center space-x-6">
-        <Link to="/" className="text-xl font-bold text-white ">Pathway Pro</Link>
-        <Link to="/" className="text-base font-normal text-white ">Explore</Link>
+        <Link to="/" className=" lg:text-4xl font-bold text-slate-100 md:text-xl">Pathway Pro</Link>
+        
+        <div className="hidden md:flex items-center space-x-6 text-slate-100">
+        <Link to="/" className=" lg:text-lg hidden md:inline text-base font-normal text-slate-100">Explore</Link>
+        </div>
       </div>
 
-
       {/* Search Bar */}
-      <div className="flex items-center border border-gray-300 rounded-lg px-3 py-1 w-1/3 bg-gray-100 focus:ring-blueprimary">
+      <div className="lg:w-wull hidden md:flex items-center border border-gray-300 rounded-lg px-3 py-1 w-1/2 bg-gray-100 focus:ring-blueprimary md:w-1/5">
         <FaSearch className="text-gray-500" />
         <input
           type="text"
           placeholder="Search Courses..."
-          className="ml-2 w-full bg-transparent focus:outline-none"
+          className="ml-1 w-full bg-transparent focus:outline-none md:text-xs"
         />
       </div>
 
-      {/* Tablet & Mobile */}
-      <div className="flex items-center space-x-6 text-white">
-        <div className="flex items-center justify-center space-x-6">
-          <Link to="/" className="text-base font-normal text-white ">Degree</Link>
-          <Link to="/" className="text-base font-normal text-white ">Careers</Link>
-          <Link to="/"> 
-          <FaShoppingCart className="hidden md:inline text-xl cursor-pointer hover:text-gray-400 transition" />
-          </Link>
-          <Link to="/"> 
-          <FaBell className="hidden md:inline text-xl cursor-pointer hover:text-gray-400 transition" />
-          </Link>
-          <Link to="/"> 
-          <FaGlobe className="hidden md:inline text-xl cursor-pointer hover:text-gray-400 transition" />
-          </Link>
-          <Link to="/login">
-            <FaUserCircle className="hidden md:inline text-xl cursor-pointer hover:text-gray-400 transition" />
-          </Link>
-        </div>
-        <button onClick={() => setMenuOpen(!menuOpen)} className="text-xl md:hidden cursor-pointer">
-          {menuOpen ? <FaTimes /> : <FaBars />}
-        </button>
+      {/* Desktop Navigation */}
+      <div className="hidden md:flex items-center space-x-6 text-slate-100">
+        <Link to="/" className="lg:text-lg hidden md:inline text-base font-normal text-slate-100">Degree</Link>
+        <Link to="/" className="lg:text-lg hidden md:inline text-base font-normal text-slate-100">Career</Link>
+        <Link to="/"> 
+          <FaShoppingCart className="text-xl cursor-pointer hover:text-gray-400 transition" />
+        </Link>
+        <Link to="/"> 
+          <FaGlobe className="text-xl cursor-pointer hover:text-gray-400 transition" />
+        </Link>
+        <Link to="/"> 
+          <FaBell className="text-xl cursor-pointer hover:text-gray-400 transition" />
+        </Link>
+        <Link to="/login">
+          <FaUserCircle className="text-xl cursor-pointer hover:text-gray-400 transition" />
+        </Link>
       </div>
 
+      {/* Mobile Menu Button */}
+      <button onClick={() => setMenuOpen(!menuOpen)} className="text-xl md:hidden cursor-pointer text-white">
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </button>
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <nav className="absolute top-16 right-0 w-full text-center bg-white shadow-md p-4 flex flex-col space-y-4 md:hidden z-10">
-          <Link to="/" className="hover:text-gray-800" onClick={() => setMenuOpen(false)}>Home</Link>
-          <Link to="/" className="hover:text-gray-800" onClick={() => setMenuOpen(false)}>Shop</Link>
-          <Link to="/login" className="hover:text-gray-800" onClick={() => setMenuOpen(false)}>Profile</Link>
-          <Link to="/" className="hover:text-gray-800" onClick={() => setMenuOpen(false)}>Notification</Link>
-          <Link to="/" className="hover:text-gray-800" onClick={() => setMenuOpen(false)}>Language</Link>
+        <nav className="absolute top-16 right-0 w-full text-center bg-slate-100 shadow-md p-4 flex flex-col space-y-4 md:hidden z-20">
+          <Link to="/" className="hover:text-slate-100 hover:bg-bluedark  " onClick={() => setMenuOpen(false)}>Home</Link>
+          <Link to="/" className="hover:text-slate-100 hover:bg-bluedark " onClick={() => setMenuOpen(false)}>Explore</Link>
+          <Link to="/" className="hover:text-slate-100 hover:bg-bluedark " onClick={() => setMenuOpen(false)}>Degree</Link>
+          <Link to="/" className="hover:text-slate-100 hover:bg-bluedark" onClick={() => setMenuOpen(false)}>Career</Link>
+          <Link to="/" className="hover:text-slate-100 hover:bg-bluedark" onClick={() => setMenuOpen(false)}>Shop</Link>
+          <Link to="/" className="hover:text-slate-100 hover:bg-bluedark" onClick={() => setMenuOpen(false)}>Notification</Link>
+          <Link to="/" className="hover:text-slate-100 hover:bg-bluedark " onClick={() => setMenuOpen(false)}>Language</Link>
+          <Link to="/login" className="hover:text-slate-100 hover:bg-bluedark" onClick={() => setMenuOpen(false)}>Profile</Link>
         </nav>
       )}
     </header>
@@ -66,4 +68,3 @@ const Header = () => {
 };
 
 export default Header;
-
--- pathwaypro/src/components/HeroBanner.jsx
+++ pathwaypro/src/components/HeroBanner.jsx
@@ -1,39 +1,31 @@
 import React from 'react';
-import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Img1 from "../assets/photo-1.png";
 
 const HeroBanner = () => {
     return (
-        <div
-            className="relative py-20 flex flex-col justify-center items-center bg-blueprimary">
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+        <div className="relative py-20 flex justify-evenly items-center bg-slate-100 p-4">
 
             {/* Content */}
-            <div className="relative z-10 px-4 text-white">
+            <div className="sm:p-8 md:p-10 sm:pl-6 md:pl-10 text-bluedark">
                 
                 {/* Headline */}
-                <h1 className="text-3xl md:text-5xl font-bold mb-4">
+                <h1 className="text-xl sm:text-xl md:text-2xl lg:text-5xl font-bold mb-4">
                     Discover Your Path to Becoming a Pro
                 </h1>
-                <p className="text-sm md:text-lg mb-6">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro autem quod ipsam eius sed veritatis accusamus voluptatibus est corporis nulla.
+                <p className="text-xs sm:text-xs md:text-base lg:text-lg mb-6">
+                    Gain in-demand skills, learn from expert-led courses, and start your career journey today!
                 </p>
-            <button className="mainBtn">
-                    Join for Free
-                </button>
+                <Link to="/login">
+                    <button className="mainBtn cursor-pointer">Join for Free</button>
+                </Link>
 
-                {/* Search Form */}
-                <div className="bg-white rounded shadow-lg p-4 w-full flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
-                    <input
-                        type="text"
-                        placeholder="Search Courses"
-                        className="p-3 border border-gray-300 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-blueprimary w-full transition"
-                    />
-                    <button className="bg-blueprimary font-semibold text-white px-6 py-3 rounded hover:bg-bluedark flex items-center w-full md:w-auto transition">
-                        <span className="mr-2"><FaSearch /></span> Search
-                    </button>
-                </div>
             </div>
+
+
+                    <img className='relative sm:w-sm md:w-1/4 lg:w-1/4 sm:right-6 md:right-10 lg:right-11' src={Img1} alt="" />
+
+                    
         </div>
     );
 };
